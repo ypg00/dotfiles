366bd45967b22297a88fcc9f190656bbe2b6c78c
Yuriy Golota <ypg@ypgmbp.local>
2024-06-09 00:00:17 +0800

    refactor: cleanup, alphabetize

--- .install/symlink.sh
+++ .install/symlink.sh
@@ -2,28 +2,20 @@
 
 # Script to create symlinks to all dotfiles
 
+echo "----- Symlinking .dotfiles -----"
+cd $HOME || exit
 CONFIG_DIR="$HOME/.config"
 DOTFILES="$HOME/.dotfiles"
-
-# Ensure .config directory exists
-mkdir -p "$CONFIG_DIR"
+mkdir -p "$CONFIG_DIR" # Ensure .config directory exists
 
 create_symlink() {
   local src="$1"
   local dest="$2"
-
   echo "Symlinking $src to $dest"
   ln -sfv "$src" "$dest"
 }
 
-# .zshrc
-create_symlink "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
-
-# nvim
-create_symlink "$DOTFILES/nvim" "$CONFIG_DIR/nvim"
-
-# karabiner
-create_symlink "$DOTFILES/karabiner" "$CONFIG_DIR/karabiner"
-
-# asdf
 create_symlink "$DOTFILES/asdf/.global-tool-versions" "$HOME/.tool-versions"
+create_symlink "$DOTFILES/karabiner" "$CONFIG_DIR/karabiner"
+create_symlink "$DOTFILES/nvim" "$CONFIG_DIR/nvim"
+create_symlink "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
