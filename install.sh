35a7204bf05036fb66355c4d82752d533d842f3b
Yuriy Golota <ypg@ypgmbp.local>
2024-06-08 17:51:43 +0800

    feat: initial commit for install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Script to create symlinks to all dotfiles
+
+CONFIG_DIR="$HOME/.config"
+DOTFILES="$HOME/.dotfiles"
+
+# Ensure .config directory exists
+mkdir -p "$CONFIG_DIR"
+
+create_symlink() {
+  local src="$1"
+  local dest="$2"
+
+  echo "Symlinking $src to $dest"
+  ln -sfv "$src" "$dest"
+}
+
+# .zshrc
+create_symlink "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
+
+# nvim
+create_symlink "$DOTFILES/nvim" "$CONFIG_DIR/nvim"
+
+# karabiner
+create_symlink "$DOTFILES/karabiner" "$CONFIG_DIR/karabiner"
+
+# abbr
+echo "Updating ABBR_USER_ABBREVIATIONS_FILE env var for zsh-abbr"
+export ABBR_USER_ABBREVIATIONS_FILE="$DOTFILES/zsh-abbr/abbr"
+
+source "$HOME/.zshrc"
