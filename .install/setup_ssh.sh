62830d8f064b512c9d3162a33850f02fdb2c1425
Yuriy Golota <ypg@ypgmbp.local>
2024-06-08 20:43:15 +0800

    refactor: add better comments and error handling

--- .install/setup_ssh.sh
+++ .install/setup_ssh.sh
@@ -12,36 +12,35 @@ if ! [[ "$email" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
     exit 1
 fi
 
-echo "Enter a name for the keys (e.g. github-personal or bitbucket-work):"
+echo "Enter a name for the keys (e.g., github-personal or bitbucket-work):"
 read key_name
 if [[ -z "$key_name" ]]; then
     echo "Key name cannot be empty. Exiting."
     exit 1
 fi
 
-
 cd $HOME || exit
 mkdir -p .ssh
 chmod 700 $HOME/.ssh
 
-local private_key_path="$HOME/.ssh/$key_name"
-ssh-keygen -t ed25519 -C "$email" -f "$private_key_path" -N "" || exit
+private_key_path="$HOME/.ssh/$key_name"
+ssh-keygen -t ed25519 -C "$email" -f "$private_key_path" -N "" || { echo "SSH key generation failed. Exiting."; exit 1; }
 chmod 600 $private_key_path
 
 echo "Starting up SSH Agent"
 eval "$(ssh-agent -s)" || true
 
 # Output SSH config
-local ssh_config="$HOME/.ssh/config"
+ssh_config="$HOME/.ssh/config"
 {
     echo "Host github.com"
-    echo "  HostName $key_name"
+    echo "  HostName github.com"
     echo "  User git"
     echo "  IdentityFile $private_key_path"
     echo "  AddKeysToAgent yes"
 } >> "$ssh_config"
-chmod 600 $ssh_config
+chmod 600 "$ssh_config"
 
-echo "Intructions on how to add you public key ($key_name.pub) to GitHub:"
+echo -e "\nInstructions on how to add your public key ($key_name.pub) to GitHub:"
 echo "https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account"
-echo \n"Public Key: $private_key_path.pub"
+echo -e "\nPublic Key:\n$(cat "$private_key_path.pub")"
