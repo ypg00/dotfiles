b3ee3dc187c77fb3c87c6b9c42ce88f47f2eb076
Yuriy Golota <ypg@ypgmbp.local>
2024-06-17 13:03:49 +0800

    chore: improve comments

--- .install/symlink.sh
+++ .install/symlink.sh
@@ -6,7 +6,7 @@
 # abbr: env var is set in .zshrc that points to dotfiles
 # asdf: handled by asdf script in order to stay synced
 
-echo "----- Symlinking .dotfiles -----"
+echo "===== Symlinking .dotfiles ====="
 cd $HOME || exit
 CONFIG_DIR="$HOME/.config"
 DOTFILES="$HOME/.dotfiles"
@@ -23,3 +23,5 @@ create_symlink "$DOTFILES/git/.global-gitconfig" "$HOME/.gitconfig"
 create_symlink "$DOTFILES/karabiner" "$CONFIG_DIR/karabiner"
 create_symlink "$DOTFILES/nvim" "$CONFIG_DIR/nvim"
 create_symlink "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
+
+echo "===== Finished symlinking to .dotfiles ====="
