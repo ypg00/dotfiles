8cd9fbfd5c23d000bf9d3405daa9dbbaec0b63a1
Yuriy Golota <98235230+ypg00@users.noreply.github.com>
2024-06-17 22:02:12 +0800

    feat: add linter plugins

--- nvim/lua/kickstart/plugins/lint.lua
+++ nvim/lua/kickstart/plugins/lint.lua
@@ -6,6 +6,8 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
+        dockerfile = { 'hadolint' },
+        json = { 'jsonlint' },
         markdown = { 'markdownlint' },
       }
 
