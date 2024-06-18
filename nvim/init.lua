12e89d3f98b853e0cd130bc1d6ba19f59c4a5cff
Yuriy Golota <98235230+ypg00@users.noreply.github.com>
2024-06-19 00:33:35 +0800

    feat: python linters

--- nvim/init.lua
+++ nvim/init.lua
@@ -510,7 +510,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -549,9 +549,12 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
+        'black',
+        'isort',
         'stylua', -- Used to format Lua code
+        'pyright',
       })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+      require('mason-tool-installer').setup { ensure_installed = ensure_installed, auto_undate = false, run_on_start = true }
 
       require('mason-lspconfig').setup {
         handlers = {
@@ -596,7 +599,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black', 'pyright' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
