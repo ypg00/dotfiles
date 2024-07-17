9e5db7128f7c9779177bbd22b90ebc7dec91f77d
Yuriy Golota <98235230+ypg00@users.noreply.github.com>
2024-07-17 10:46:46 +0800

    wip: tab/spaces

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,6 +24,12 @@ vim.keymap.set('n', '<Space>n', toggle_numbers, { desc = 'Toggle line [N]umbers'
 -- Do not wrap text onto what looks like a new line
 vim.opt.wrap = false
 
+-- Tab == 4 spaces
+-- vim.opt.tabstop = 8
+-- vim.opt.softtabstop = 4
+-- vim.opt.shiftwidth = 4
+-- vim.opt.expandtab = false
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
