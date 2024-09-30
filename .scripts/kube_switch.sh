bd0e27ba95ab37452353530ca743f468cf6cf424
Yuriy Golota <yuriy.golota@mann-hummel.com>
2024-09-30 16:27:19 +0800

    feat: improve workflows runnings printout

--- .scripts/kube_switch.sh
+++ .scripts/kube_switch.sh
@@ -3,3 +3,4 @@
 # Switch between Kubernetes enviroments
 aws --profile $1 eks update-kubeconfig --region eu-central-1 --name IO-$1
 aws --profile $1 sts get-caller-identity
+kubectl get workflows -A -o jsonpath='{range .items[?(@.status.phase=="Running")]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.status.phase}{"\n"}{end}'
