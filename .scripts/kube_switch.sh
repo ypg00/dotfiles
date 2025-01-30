ae8346a39f56d918e36dd59d3f67d6abe327f050
Yuriy Golota <98235230+ypg00@users.noreply.github.com>
2025-01-31 04:47:00 +0800

    update

--- .scripts/kube_switch.sh
+++ .scripts/kube_switch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Switch between Kubernetes enviroments
-aws --profile $1 eks update-kubeconfig --region eu-central-1 --name 1-31-$1
+aws eks update-kubeconfig --region eu-central-1 --name 1-31-$1 --profile $1
 aws --profile $1 sts get-caller-identity
 kubectl get workflows -A -o jsonpath='{range .items[?(@.status.phase=="Running")]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.status.phase}{"\n"}{end}'
