f2aca2f65711819fec31829f6a6727994349ecdd
Yuriy Golota <98235230+ypg00@users.noreply.github.com>
2025-02-01 17:16:04 +0800

    feat: update kubeconfig update func to pass two args for cluster/env switching

--- .scripts/kube_switch.sh
+++ .scripts/kube_switch.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 # Switch between Kubernetes enviroments
-aws eks update-kubeconfig --region eu-central-1 --name 1-31-$1 --profile $1
-aws --profile $1 sts get-caller-identity
+aws eks update-kubeconfig --region eu-central-1 --profile $2 --name "$1-$2"
+aws sts get-caller-identity --profile $2
+
+echo "---------- Workflows ----------"
 kubectl get workflows -A -o jsonpath='{range .items[?(@.status.phase=="Running")]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.status.phase}{"\n"}{end}'
+echo "---------- cronWorkflows ----------"
+kubectl get cronWorkflows -A -o jsonpath='{range .items[?(@.status.phase=="Running")]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.status.phase}{"\n"}{end}'
