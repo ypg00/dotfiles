42a351c65b58aa5d978714706713c768d8a275f5
Yuriy Golota <98235230+ypg00@users.noreply.github.com>
2025-03-20 18:20:12 +0800

    feat: update kube_switch script to rm deprecated cmd

--- .scripts/kube_switch.sh
+++ .scripts/kube_switch.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-# Switch between Kubernetes enviroments
 aws eks update-kubeconfig --region eu-central-1 --profile $2 --name "$1-$2"
 aws sts get-caller-identity --profile $2
-kubectl config view | rg current
 
-echo "---------- Workflows ----------"
+echo "-------------------- Workflows --------------------"
 kubectl get workflows -A -o jsonpath='{range .items[?(@.status.phase=="Running")]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.status.phase}{"\n"}{end}'
-echo "---------- cronWorkflows ----------"
-kubectl get cronWorkflows -A -o jsonpath='{range .items[?(@.status.phase=="Running")]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.status.phase}{"\n"}{end}'
+echo "---------------------------------------------------"
+kubectl config view | rg current
