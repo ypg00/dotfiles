73467b6232c87204056104da737e2cb50771fc4f
Yuriy Golota <98235230+ypg00@users.noreply.github.com>
2025-05-02 17:35:02 +0800

    feat: update for just v3 clusters as IO clusters have been deleted

--- .scripts/kube_switch.sh
+++ .scripts/kube_switch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-aws eks update-kubeconfig --region eu-central-1 --profile $2 --name "$1-$2"
-aws sts get-caller-identity --profile $2
+aws eks update-kubeconfig --region eu-central-1 --profile $1 --name "v3-$1"
+aws sts get-caller-identity --profile $1
 
 echo "-------------------- Workflows --------------------"
 kubectl get workflows -A -o jsonpath='{range .items[?(@.status.phase=="Running")]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.status.phase}{"\n"}{end}'
