db99c0be08ea644f3025d441cd52bc2b80fb6006
Yuriy Golota <98235230+ypg00@users.noreply.github.com>
2025-05-14 17:51:22 +0800

    feat: rm filters for wf print

--- .scripts/kube_switch.sh
+++ .scripts/kube_switch.sh
@@ -3,7 +3,9 @@
 aws eks update-kubeconfig --region eu-central-1 --profile $1 --name "v3-$1"
 aws sts get-caller-identity --profile $1
 
-echo "-------------------- Workflows --------------------"
-kubectl get workflows -A -o jsonpath='{range .items[?(@.status.phase=="Running")]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.status.phase}{"\n"}{end}'
-echo "---------------------------------------------------"
+echo "------------------------------- Workflows -------------------------------"
+kubectl get workflows -A
+# kubectl get workflows -A -o jsonpath='{range .items[?(@.status.phase=="Running")]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.status.phase}{"\n"}{end}'
+echo ""
 kubectl config view | rg current
+echo "-------------------------------------------------------------------------"
